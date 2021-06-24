a280c67b9f0b3dcac2678ee7982b3f6f0e381930
Jaslan <Jaslan.dlnv@gmail.com>
2021-06-24 18:46:56 +0500

    Routing

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Container, Typography, Button, Grid } from '@material-ui/core';
 
-
+import { Link } from 'react-router-dom';
 import useStyles from './styles';
 import CartItem from './CartItem/CartItem';
 
@@ -9,7 +9,11 @@ const Cart = ({ cart }) => {
    const classes = useStyles();
 
    const EmptyCart = () => (
-      <Typography variant="subtitle1">You have no items in your shopping cart, start adding some!</Typography>
+      <Typography variant="subtitle1">You have no items in your shopping cart,
+         <Link to="/" className={classes.link}>
+            start adding some!
+         </Link>
+      </Typography>
    );
 
    const FilledCart = () => (
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,25 +4,31 @@ import { ShoppingCart } from '@material-ui/icons';
 
 import logo from '../../assets/commerce.png';
 import useStyles from './styles';
+import { Link, useLocation } from 'react-router-dom';
+
 
 const Navbar = ({ totalItems }) => {
    const classes = useStyles();
+   const location = useLocation();
+
+
    return (
       <>
          <AppBar position="fixed" className={classes.appBar} color="inherit">
             <Toolbar>
-               <Typography variant='h6' className={classes.title} color="inherit">
+               <Typography component={Link} to="/" variant='h6' className={classes.title} color="inherit">
                   <img src={logo} alt="Commerce.js" height="25px" className={classes.image}/>
                   Commerce.js
                </Typography>
                <div className={classes.grow}/>
-               <div className={classes.button} >
-                  <IconButton aria-label="Show cart items" color="inherit">
-                     <Badge badgeContent={ totalItems } color="secondary">
-                        <ShoppingCart />
-                     </Badge>
-                  </IconButton>
-               </div>
+               {location.pathname === '/' && (
+                  <div className={classes.button}>
+                     <IconButton component={Link} to="/cart" aria-label="Show cart items" color="inherit">
+                        <Badge badgeContent={ totalItems } color="secondary">
+                           <ShoppingCart />
+                        </Badge>
+                     </IconButton>
+                  </div>)}
             </Toolbar>
          </AppBar>
       </>
