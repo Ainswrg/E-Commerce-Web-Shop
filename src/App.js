b315111874e3dc801c69ff0aac054deddb9d41dc
Jaslan <Jaslan.dlnv@gmail.com>
2021-06-24 17:03:13 +0500

    Commerce Products

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React from 'react';
-
+import React, { useState, useEffect } from 'react';
+import { commerce } from './lib/commerce';
 import { Products, Navbar } from './components';
 
 const App = () => {
+   const [products, setProducts] = useState([]);
+
+
+   const fetchProducts = async () => {
+      const { data } = await commerce.products.list();
+
+      setProducts(data);
+   }
+
+ 
+
+   const handleAddToCart = async (productId, quantity) => {
+      // const response = await commerce.cart.add(productId, quantity);
+      const item = await commerce.cart.add(productId, quantity);
+
+   }
+
+   useEffect(() => {
+      fetchProducts();
+   },[]);
+
+
    return (
       <div>
          <Navbar />
-         <Products />
+         <Products products={products} onAddToCart={handleAddToCart} />
+
       </div>
    )
 }
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,12 +4,7 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 import useStyles from './styles';
 
-const products = [
-   {id: 1, name: 'Shoes', description: 'Running shoes.', price: '$5', image: 'https://5.imimg.com/data5/KF/QF/TG/ANDROID-94833360/product-jpeg-500x500.jpeg'},
-   {id: 2, name: 'Macbook', description: 'Apple macbook.', price: '$10', image: 'https://www.techadvisor.com/cmsdata/features/3802413/new-apple-macbook-air-2021-release-date-price-spec-rumours-main_thumb800.png'},
-]
-
-const Products = () => {
+const Products = ({ products, onAddToCart }) => {
    const classes = useStyles();
 
    return (
@@ -18,12 +13,12 @@ const Products = () => {
          <Grid container justify="center" spacing={4}>
             {products.map((product) => (
                <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                  <Product product={product} />
+                  <Product product={product} onAddToCart={onAddToCart} />
                </Grid>
             ))}
          </Grid>
       </main>
-   )
+   );
 }
 
 export default Products;
