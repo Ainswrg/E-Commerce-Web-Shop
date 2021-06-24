368c5b43cd2d5ed6338e3d37b92cbe9a276f20e7
Jaslan <Jaslan.dlnv@gmail.com>
2021-06-24 18:03:19 +0500

    Add card, card layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { commerce } from './lib/commerce';
-import { Products, Navbar } from './components';
+import { Products, Navbar, Cart } from './components';
 
 const App = () => {
    const [products, setProducts] = useState([]);
-
+   const [cart, setCart] = useState([]);
 
    const fetchProducts = async () => {
       const { data } = await commerce.products.list();
@@ -12,24 +12,29 @@ const App = () => {
       setProducts(data);
    }
 
- 
+   const fetchCart = async () => {
+      setCart(await commerce.cart.retrieve());
+   }
 
    const handleAddToCart = async (productId, quantity) => {
       // const response = await commerce.cart.add(productId, quantity);
       const item = await commerce.cart.add(productId, quantity);
 
+      setCart(item.cart);
    }
 
    useEffect(() => {
       fetchProducts();
+      fetchCart();
    },[]);
 
+   console.log(cart);
 
    return (
       <div>
-         <Navbar />
-         <Products products={products} onAddToCart={handleAddToCart} />
-
+         <Navbar totalItems={cart.total_items}/>
+         {/* <Products products={products} onAddToCart={handleAddToCart} /> */}
+         <Cart cart={cart}/>
       </div>
    )
 }
